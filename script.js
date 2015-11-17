4dba723835a5321f7f2a7018f9090bc66a31191c
williamsears <wpeter1@hotmail.com>
2015-11-17 18:31:20 +0000

    Added initial script that says hello world and "hello world again!!" ten seconds after execution.

---
+++ script.js
@@ -0,0 +1,9 @@
+//This is a script in javascript
+
+function helloWorld() {
+    console.log("Hello world!")
+}
+
+setTimeout(function(){ console.log("Hello world again!!"); }, 10000);
+
+helloWorld();
